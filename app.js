34262e6564bc72c097fd23dd228d31cede32b419
Sujal Soni <sujal0soni123@gmail.com>
2025-08-30 20:07:12 +0530

    Game

---
+++ app.js
@@ -0,0 +1,60 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+const genCompChoice = () => {
+  const options = ["rock","paper","scissors"];
+  const randomIdx = Math.floor(Math.random() * 3);
+  return options[randomIdx];
+};
+
+const Draw = () => {
+  console.log("Its a tie!");
+}
+
+const playGame = (userChoice) => {
+  console.log("User Choice:" + userChoice);
+  const compChoice = genCompChoice();
+  console.log("Computer Choice:" + compChoice);
+
+
+  
+  
+  // else  {
+    
+    if(userChoice === compChoice){
+    Draw();
+    }
+    if(userChoice === "rock" && compChoice === "paper"){
+      userwin = false;
+      console.log("Computer Wins!");
+      compScore++;
+    }
+    else if(userChoice === "paper" && compChoice === "scissors"){
+      userwin = false;
+      console.log("Computer Wins!");
+      compScore++;
+    }
+    else if(userChoice === "scissors" && compChoice === "rock"){
+      userwin = false;
+      console.log("Computer Wins!");
+      compScore++;
+    }
+
+    return compChoice;
+  };
+//  };
+
+
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+      const userChoice = choice.getAttribute("id");
+      // console.log("Choice is been clicked  " + userChoice); 
+      playGame(userChoice);
+      document.getElementById("user-score").innerText = userScore;
+      document.getElementById("comp-score").innerText = compScore;
+      document.getElementById("msg").innerText = `You chose ${userChoice}, Computer chose ${genCompChoice()}`; 
+    })
+})
